b474fc90b62be23e10fb15f5b3440c9888152b03
sporacid <sporacid@gmail.com>
2023-08-20 19:53:33 -0400

    fix: Update foonathan-cppast and foonathan-type-safe

--- ports/foonathan-cppast/portfile.cmake
+++ ports/foonathan-cppast/portfile.cmake
@@ -4,11 +4,8 @@ vcpkg_from_github(
   REF f00df6675d87c6983033d270728c57a55cd3db22
   SHA512 7d045fdbb9b7473f0b082ebb289560111542fa82e7317f8e651d921bd146f30a408725bcab7f4f9079c7bda254a6014c385614b7a6dc22f92ab379508153d53a
   HEAD_REF main
-)
-
-vcpkg_apply_patches(
-  SOURCE_PATH ${SOURCE_PATH}
-  PATCHES "0001-fixup-top-level-directory.patch"
+  PATCHES
+    0001-fixup-top-level-directory.patch
 )
 
 vcpkg_cmake_configure(
