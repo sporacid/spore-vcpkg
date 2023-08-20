6668a061be9922414995f5d8e76e689ac98f0d8f
sporacid <sporacid@gmail.com>
2023-08-20 15:34:18 -0400

    fix: Update spore-semver REF

--- ports/spore-semver/portfile.cmake
+++ ports/spore-semver/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO sporacid/spore-semver
-  REF 5642a6548e18f26f1835bd708478e3aa64a5925e
+  REF 29039a57b06e43cb58a1d8cb0eee50a5ef9fe10e
   SHA512 e4bf8f92fa6a3c8abba02c9f583ccf092825a5bd23c2bd7faaa5a57d99ecc36e9c87eb94939b661d3f1871bb0ccee5d17438257eb5e350118ffaea7af5bfc836
   HEAD_REF main
 )
