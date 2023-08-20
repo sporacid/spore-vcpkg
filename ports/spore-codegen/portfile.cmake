1dced483222a311b4049a082f103e9b7a7885248
sporacid <sporacid@gmail.com>
2023-08-20 12:06:12 -0400

    feat: Initial commit

---
+++ ports/spore-codegen/portfile.cmake
@@ -0,0 +1,15 @@
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO sporacid/spore-codegen
+  # REF 19a1f95c2f56a27ced90227b5e2754a602a08e69
+  # SHA512 7b2bb7acb2a8ff07bff59cfa27247a7b2cced03828919cd65cc0c8cf1f724f5f1e947ed6992dcdbc913fb470694a52613d1861eaaadbf8903e94eb9cdfe4d000
+  HEAD_REF main
+)
+
+vcpkg_configure_cmake(
+  SOURCE_PATH "${SOURCE_PATH}"
+  PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+vcpkg_cmake_config_fixup()
