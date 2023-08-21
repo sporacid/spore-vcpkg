a208fffa330af9c319ebea7f4e97283b73a39852
sporacid <sporacid@gmail.com>
2023-08-20 20:24:40 -0400

    fix: Update p-ranav-glob

--- ports/p-ranav-glob/portfile.cmake
+++ ports/p-ranav-glob/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO p-ranav/glob
   REF 8cd16218ae648addf166977d7e41b914768bcb05
-  SHA512 52a1e3aa2320d63ebe028db40b8db26ff7b7e5ab67813eaf339e9d994bda7068e47ef940570f7304c0b8f649455d1bc3f51df3205e24522e0c88f6004faf1ab9
+  SHA512 a2c7f2c4d43a583d3915d3f9fd0b6e0be48930f455b47c9559cf641352020c545f6d0ddc4ea8f6b8d5a4f54a6cc3b1a05295a6aa5d83a398758b69b12072adcb
   HEAD_REF master
 )
 
@@ -11,4 +11,3 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-# vcpkg_cmake_config_fixup()
