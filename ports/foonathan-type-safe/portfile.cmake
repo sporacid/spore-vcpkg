2b2957df05c3a5051d33253cea34e9750dd3aaf7
sporacid <sporacid@gmail.com>
2023-08-21 09:48:24 -0400

    fix: Update foonathan-type-safe and foonathan-cppast

--- ports/foonathan-type-safe/portfile.cmake
+++ ports/foonathan-type-safe/portfile.cmake
@@ -11,7 +11,10 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/type_safe)
+vcpkg_cmake_config_fixup(
+  PACKAGE_NAME type_safe
+  CONFIG_PATH lib/cmake/type_safe
+)
 
 file(
   REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
