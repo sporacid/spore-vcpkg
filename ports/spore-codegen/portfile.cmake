8ea016abb3d1252b7363103dc456812b5c463291
sporacid <sporacid@gmail.com>
2023-08-21 12:23:22 -0400

    fix: Update spore-codegen

--- ports/spore-codegen/portfile.cmake
+++ ports/spore-codegen/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO sporacid/spore-codegen
-  REF 4335ccc20bee3e75689337c219da019bc179f92e
+  REF 02d0a7a4d24e72feeb1bdaab08ab74f2682e3278
   SHA512 52a1e3aa2320d63ebe028db40b8db26ff7b7e5ab67813eaf339e9d994bda7068e47ef940570f7304c0b8f649455d1bc3f51df3205e24522e0c88f6004faf1ab9
   HEAD_REF main
 )
@@ -9,20 +9,14 @@ vcpkg_from_github(
 vcpkg_cmake_configure(
   SOURCE_PATH ${SOURCE_PATH}
   OPTIONS
-  -DWITH_TESTS=OFF
-  -DWITH_EXAMPLES=OFF
-  -DWITH_COMPILE_COMMANDS=OFF
-  -DFETCHCONTENT_FULLY_DISCONNECTED=OFF
-  -DBUILD_SHARED_LIBS=OFF
+    -DSPORE_BUILD_EXAMPLES=OFF
+    -DSPORE_BUILD_TESTS=OFF
+    -DSPORE_COMPILE_COMMANDS=OFF
 )
 
 vcpkg_cmake_install()
 
 file(
-  INSTALL ${SOURCE_PATH}/cmake/spore_codegen.cmake
+  INSTALL ${SOURCE_PATH}/cmake/SporeCodegen.cmake
   DESTINATION ${CURRENT_PACKAGES_DIR}/share/spore
-)
-
-vcpkg_cmake_config_fixup(
-  PACKAGE_NAME "spore.codegen"
 )
