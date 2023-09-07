97308fc39a50a9332d6be2063f44e552ce2a6e24
sporacid <sporacid@gmail.com>
2023-09-07 18:50:29 -0400

    fix: Update bombomby-optick

--- ports/bombomby-optick/portfile.cmake
+++ ports/bombomby-optick/portfile.cmake
@@ -12,9 +12,12 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(
+  CONFIG_PATH lib/cmake/Optick
+  PACKAGE_NAME Optick
+)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 vcpkg_copy_pdbs()
 
