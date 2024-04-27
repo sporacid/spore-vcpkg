c6f3ad2834f534ee9b86800121e4dfc5edfc7bec
sporacid <sporacid@gmail.com>
2024-04-27 09:23:56 -0400

    fix: Update spore-codegen REF

--- ports/spore-codegen/portfile.cmake
+++ ports/spore-codegen/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO sporacid/spore-codegen
-  REF 3a5174db30b286b09c4ae6bd40a2f6856b365d45
+  REF c1e611f0dfd48d93b2aa02aa7543ff96e861cd30
   SHA512 b6cb353b1f7ee0cc73fc2b05a680466a892d3bf97af433c9bbcb38962ab1c36f73ca6f294389a37ca26377fbde89ec583a4dde559a4bae4d93cc5343a924cec9
   HEAD_REF main
 )
