332222c059a16a4e0eda67bc18c3d25b1fb78c97
sporacid <sporacid@gmail.com>
2025-07-22 16:07:53 -0400

    feat: Added new port for spore-meta

---
+++ ports/spore-meta/portfile.cmake
@@ -0,0 +1,48 @@
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO sporacid/spore-meta
+  REF 40bdd37b1a1cffd8ceb48764fe06628f48386130
+  SHA512 TODO
+  HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+  SOURCE_PATH ${SOURCE_PATH}
+  OPTIONS
+    -DSPORE_BUILD_EXAMPLES=OFF
+)
+
+vcpkg_cmake_install()
+
+vcpkg_install_copyright(
+  FILE_LIST ${SOURCE_PATH}/LICENSE
+)
+
+configure_file(
+  ${CMAKE_CURRENT_LIST_DIR}/config.cmake.in
+  ${CURRENT_PACKAGES_DIR}/share/${PORT}/${PORT}-config.cmake
+  @ONLY
+)
+
+file(
+  INSTALL ${SOURCE_PATH}/cmake/
+  DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+)
+
+file(
+  INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage
+  DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+)
+
+file(
+  REMOVE_RECURSE
+  ${CURRENT_PACKAGES_DIR}/cmake
+  ${CURRENT_PACKAGES_DIR}/debug
+)
+
+if (EXISTS ${SOURCE_PATH}/VERSION)
+  file(
+    INSTALL ${SOURCE_PATH}/VERSION
+    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+  )
+endif ()
