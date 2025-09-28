14f9aec802b8f06903173731e4c25c9309ee6da8
sporacid <sporacid@gmail.com>
2025-09-27 22:21:23 -0400

    feat: Updated spore-proxy portfile

--- ports/spore-proxy/portfile.cmake
+++ ports/spore-proxy/portfile.cmake
@@ -9,6 +9,10 @@ vcpkg_from_github(
 vcpkg_cmake_configure(
   SOURCE_PATH ${SOURCE_PATH}
   DISABLE_PARALLEL_CONFIGURE
+  OPTIONS
+    -DSPORE_WITH_BENCHMARKS=OFF
+    -DSPORE_WITH_EXAMPLES=OFF
+    -DSPORE_WITH_TESTS=OFF
 )
 
 vcpkg_cmake_install()
